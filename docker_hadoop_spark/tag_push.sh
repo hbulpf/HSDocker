e89201b36fe365c344c5260535e2e74a81f37bec
LPF-OFFICE <hudalpf@163.com>
2019-03-08 15:29:45 +0800

    tag push

--- docker_hadoop_spark/tag_push.sh
+++ docker_hadoop_spark/tag_push.sh
@@ -1,9 +1,12 @@
 docker tag hs_hadoop:v1.0 202.116.46.215/hsdocker2019/hs_hadoop:v1.0 
+docker push 202.116.46.215/hsdocker2019/hs_hadoop:v1.0 
+
+docker tag hs_hive:v1.0 202.116.46.215/hsdocker2019/hs_hive:v1.0  
+docker push 202.116.46.215/hsdocker2019/hs_hive:v1.0  
+
 docker tag hs_spark:v1.0  202.116.46.215/hsdocker2019/hs_spark:v1.0  
 docker tag hs_hbase:v1.0 202.116.46.215/hsdocker2019/hs_hbase:v1.0 
-docker tag hs_hive:v1.0 202.116.46.215/hsdocker2019/hs_hive:v1.0  
 
-docker push 202.116.46.215/hsdocker2019/hs_hadoop:v1.0 
 docker push 202.116.46.215/hsdocker2019/hs_spark:v1.0
 docker push 202.116.46.215/hsdocker2019/hs_hbase:v1.0 
-docker push 202.116.46.215/hsdocker2019/hs_hive:v1.0  
+
