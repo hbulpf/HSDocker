5c5dd316f2ab427da55e7060e892e2d373b5fb97
LPF-OFFICE <hudalpf@163.com>
2019-03-13 19:42:20 +0800

    storm

--- docker_hadoop_spark/tag_push.sh
+++ docker_hadoop_spark/tag_push.sh
@@ -11,3 +11,6 @@ docker push 202.116.46.215/hsdocker2019/hs_spark:v1.0
 
 docker tag hs_hbase:v1.0 202.116.46.215/hsdocker2019/hs_hbase:v1.0 
 docker push 202.116.46.215/hsdocker2019/hs_hbase:v1.0 
+
+docker tag hs_storm:v1.0 202.116.46.215/hsdocker2019/hs_storm:v1.0
+docker push 202.116.46.215/hsdocker2019/hs_storm:v1.0
