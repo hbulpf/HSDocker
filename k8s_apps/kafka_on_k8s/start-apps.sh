ee2fc4b2d3c48505c59021b66fecb05f49633323
chellyk <yaokunm@qq.com>
2019-03-16 10:14:44 +0800

    new yaml

---
+++ k8s_apps/kafka_on_k8s/start-apps.sh
@@ -0,0 +1,33 @@
+kubectl create -f storm-master.yaml -n test
+kubectl create -f storm-slave.yaml -n test
+
+#write to /etc/hosts on master
+hoststr=$(kubectl get pod -o wide -n test | grep -i "slave" | awk '{print $6"\t"$1}')
+host0="slave-0"
+host1="slave-1"
+count=0
+while :
+do
+	if [[ $hoststr =~ $host0 && $hoststr =~ $host1 && ! $hoststr =~ "<none>" ]]; then
+		echo "write to /etc/hosts"
+		kubectl get pod -o wide -n test | grep -i "slave" | awk '{print $6"\t"$1}' > hosts_tmp && \
+			kubectl cp hosts_tmp master:/tmp/hosts_tmp -n test && \
+			kubectl exec master -n test -- /bin/sh -c "cat /tmp/hosts_tmp >> /etc/hosts"	
+		cat hosts_tmp	
+		break
+	fi
+	sleep 1
+	((count++))
+	echo "$count s:$hoststr"
+	hoststr=$(kubectl get pod -o wide -n test | grep -i "slave" | awk '{print $6"\t"$1}')
+done
+
+rm hosts_tmp
+# scp /etc/hosts to slaves
+kubectl exec master -n test -- scp /etc/hosts slave-0:/etc/hosts
+kubectl exec master -n test -- scp /etc/hosts slave-1:/etc/hosts
+
+
+kubectl exec master -n test -- /bin/sh -c 'echo "1" > /usr/local/zookeeper/data/myid'
+kubectl exec slave-0 -n test -- /bin/sh -c 'echo "2" > /usr/local/zookeeper/data/myid'
+kubectl exec slave-1 -n test -- /bin/sh -c 'echo "3" > /usr/local/zookeeper/data/myid'
