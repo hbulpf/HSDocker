16b6f8d73d0aba00b005c6b3c6e6a7aaa18ec4a4
LPF-OFFICE <hudalpf@163.com>
2019-03-17 12:22:42 +0800

    dl push pull

---
+++ deeplearning/tag_pull.sh
@@ -0,0 +1,16 @@
+#tensorflow:1.12.0-gpu-py3
+docker pull 202.116.46.215/hsdocker2019/tensorflow:1.12.0-gpu-py3
+docker tag 202.116.46.215/hsdocker2019/tensorflow:1.12.0-gpu-py3 tensorflow/tensorflow:1.12.0-gpu-py3
+
+#tensorflow:1.12.0-py3
+docker pull 202.116.46.215/hsdocker2019/tensorflow:1.12.0-py3
+docker tag 202.116.46.215/hsdocker2019/tensorflow:1.12.0-py3 tensorflow/tensorflow:1.12.0-py3 
+
+#tensorflow:1.12.0-gpu
+docker pull 202.116.46.215/hsdocker2019/tensorflow:1.12.0-gpu
+docker tag 202.116.46.215/hsdocker2019/tensorflow:1.12.0-gpu tensorflow/tensorflow:1.12.0-gpu 
+
+#tensorflow:1.12.0
+docker pull 202.116.46.215/hsdocker2019/tensorflow:1.12.0
+docker tag 202.116.46.215/hsdocker2019/tensorflow:1.12.0 tensorflow/tensorflow:1.12.0 
+
---
+++ deeplearning/tag_push.sh
@@ -0,0 +1,16 @@
+#tensorflow:1.12.0-gpu-py3
+docker tag tensorflow/tensorflow:1.12.0-gpu-py3 202.116.46.215/hsdocker2019/tensorflow:1.12.0-gpu-py3 
+docker push 202.116.46.215/hsdocker2019/tensorflow:1.12.0-gpu-py3
+
+#tensorflow:1.12.0-py3
+docker tag tensorflow/tensorflow:1.12.0-py3 202.116.46.215/hsdocker2019/tensorflow:1.12.0-py3
+docker push 202.116.46.215/hsdocker2019/tensorflow:1.12.0-py3
+
+#tensorflow:1.12.0-gpu
+docker tag tensorflow/tensorflow:1.12.0-gpu 202.116.46.215/hsdocker2019/tensorflow:1.12.0-gpu
+docker push 202.116.46.215/hsdocker2019/tensorflow:1.12.0-gpu
+
+#tensorflow:1.12.0
+docker tag tensorflow/tensorflow:1.12.0 202.116.46.215/hsdocker2019/tensorflow:1.12.0
+docker push 202.116.46.215/hsdocker2019/tensorflow:1.12.0
+
