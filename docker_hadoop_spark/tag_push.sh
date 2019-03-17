40d17526fe027d753b6ad6520d4581876c7d9a4a
LPF-OFFICE <hudalpf@163.com>
2019-03-17 21:37:57 +0800

    s

--- docker_hadoop_spark/tag_push.sh
+++ docker_hadoop_spark/tag_push.sh
@@ -38,3 +38,4 @@ docker push 202.116.46.215/hsdocker2019/hs_mongodb:v1.0
 
 docker tag hs_leveldb:v1.0 202.116.46.215/hsdocker2019/hs_leveldb:v1.0
 docker push 202.116.46.215/hsdocker2019/hs_leveldb:v1.0
+
