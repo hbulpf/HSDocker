f4a08bfd570cb00594e4787d3d4852a0e415d528
LPF-OFFICE <hudalpf@163.com>
2019-03-22 15:28:35 +0800

    将 test 换成参数

--- k8s_apps/hadoop_on_k8s/v4.1/start-apps.sh
+++ k8s_apps/hadoop_on_k8s/v4.1/start-apps.sh
@@ -1,21 +1,16 @@
 #delete resources if exists
-kubectl delete -f . -n test
-kubectl delete configmap hadoop-config -n test
-kubectl delete -f ./nfs/slave-pv.yaml
-kubectl delete -f ./nfs/slave-pvc.yaml
-kubectl delete -f ./nfs/master-pv.yaml
-kubectl delete -f ./nfs/master-pvc.yaml
+NS=$1
+kubectl delete -f . -n $NS
+kubectl delete -f ./nfs/
+
 #create resources
-kubectl create -f ./nfs/master-pv.yaml
-kubectl create -f ./nfs/master-pvc.yaml
-kubectl create -f ./nfs/slave-pv.yaml
-kubectl create -f ./nfs/slave-pvc.yaml
-kubectl create configmap hadoop-config -n test --from-file=./hadoop_configmap
-kubectl create -f hadoop-master.yaml -n test
-kubectl create -f hadoop-slave.yaml -n test
-kubectl create -f hadoop-web.yaml -n test
+kubectl create -f ./nfs/
+kubectl create configmap hadoop-config -n $NS --from-file=./hadoop_configmap
+kubectl create -f hadoop-master.yaml -n $NS
+kubectl create -f hadoop-slave.yaml -n $NS
+kubectl create -f hadoop-web.yaml -n $NS
 #write to /etc/hosts on master
-hoststr=$(kubectl get pod -o wide -n test | grep -i "slave" | awk '{print $6"\t"$1}')
+hoststr=$(kubectl get pod -o wide -n $NS | grep -i "slave" | awk '{print $6"\t"$1}')
 host0="slave-0"
 host1="slave-1"
 count=0
@@ -23,19 +18,19 @@ while :
 do
 	if [[ $hoststr =~ $host0 && $hoststr =~ $host1 && ! $hoststr =~ "<none>" ]]; then
 		echo "write to /etc/hosts"
-		kubectl get pod -o wide -n test | grep -i "slave" | awk '{print $6"\t"$1}' > hosts_tmp && \
-			kubectl cp hosts_tmp master:/tmp/hosts_tmp -n test && \
-			kubectl exec master -n test -- /bin/sh -c "cat /tmp/hosts_tmp >> /etc/hosts"	
+		kubectl get pod -o wide -n $NS | grep -i "slave" | awk '{print $6"\t"$1}' > hosts_tmp && \
+			kubectl cp hosts_tmp master:/tmp/hosts_tmp -n $NS && \
+			kubectl exec master -n $NS -- /bin/sh -c "cat /tmp/hosts_tmp >> /etc/hosts"	
 		cat hosts_tmp	
 		break
 	fi
 	sleep 1
 	((count++))
 	echo "$count s:$hoststr"
-	hoststr=$(kubectl get pod -o wide -n test | grep -i "slave" | awk '{print $6"\t"$1}')
+	hoststr=$(kubectl get pod -o wide -n $NS | grep -i "slave" | awk '{print $6"\t"$1}')
 done
 rm hosts_tmp
-kubectl exec master -n test -- scp /etc/hosts slave-0:/etc/hosts
-kubectl exec master -n test -- scp /etc/hosts slave-1:/etc/hosts
+kubectl exec master -n $NS -- scp /etc/hosts slave-0:/etc/hosts
+kubectl exec master -n $NS -- scp /etc/hosts slave-1:/etc/hosts
 # start hadoop
-kubectl exec master -n test -- start-all.sh
+kubectl exec master -n $NS -- start-all.sh
