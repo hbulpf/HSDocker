92a680f4c1fa8472fca86746e200466f7dd67491
LPF-OFFICE <hudalpf@163.com>
2019-03-23 12:16:07 +0800

    修改错误

--- docker_hadoop_spark/tag_push.sh
+++ docker_hadoop_spark/tag_push.sh
@@ -34,8 +34,8 @@ docker push 202.116.46.215/hsdocker2019/hs_redis:v1.0
 docker tag hs_redis-spark:v1.0 202.116.46.215/hsdocker2019/hs_redis-spark:v1.0
 docker push 202.116.46.215/hsdocker2019/hs_redis-spark:v1.0
 
-docker tag mahout-hadoop:v1.0 202.116.46.215/hsdocker2019/mahout-hadoop:v1.0
-docker push 202.116.46.215/hsdocker2019/mahout-hadoop:v1.0
+docker tag hs_mahout-hadoop:v1.0 202.116.46.215/hsdocker2019/hs_mahout-hadoop:v1.0
+docker push 202.116.46.215/hsdocker2019/hs_mahout-hadoop:v1.0
 
 docker tag hs_mongodb:v1.0 202.116.46.215/hsdocker2019/hs_mongodb:v1.0
 docker push 202.116.46.215/hsdocker2019/hs_mongodb:v1.0
