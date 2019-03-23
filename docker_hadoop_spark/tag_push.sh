ff4fea04344a27cc4bc386b018cb435e6ab7af2a
LPF-OFFICE <hudalpf@163.com>
2019-03-23 12:12:40 +0800

    修改错误

--- docker_hadoop_spark/tag_push.sh
+++ docker_hadoop_spark/tag_push.sh
@@ -13,8 +13,8 @@ docker push 202.116.46.215/hsdocker2019/hs_zk-hadoop:v1.0
 docker tag hs_zk:v1.0  202.116.46.215/hsdocker2019/hs_zk:v1.0  
 docker push 202.116.46.215/hsdocker2019/hs_zk:v1.0
 
-docker tag hbase-zk-hadoop:v1.0 202.116.46.215/hsdocker2019/hbase-zk-hadoop:v1.0 
-docker push 202.116.46.215/hsdocker2019/hbase-zk-hadoop:v1.0 
+docker tag hs_hbase-zk-hadoop:v1.0 202.116.46.215/hsdocker2019/hs_hbase-zk-hadoop:v1.0 
+docker push 202.116.46.215/hsdocker2019/hs_hbase-zk-hadoop:v1.0 
 
 docker tag hs_storm-zk:v1.0 202.116.46.215/hsdocker2019/hs_storm-zk:v1.0
 docker push 202.116.46.215/hsdocker2019/hs_storm-zk:v1.0
