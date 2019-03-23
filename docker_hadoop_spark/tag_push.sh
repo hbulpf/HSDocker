9b76acb1117d7263b2555fbfb30860a7389988ef
LPF-OFFICE <hudalpf@163.com>
2019-03-23 11:23:35 +0800

    修复错误

--- docker_hadoop_spark/tag_push.sh
+++ docker_hadoop_spark/tag_push.sh
@@ -1,15 +1,14 @@
-docker tag hs_base:v1.0 202.116.46.215/hsdocker2019/hs_base:v1.0 
-docker push 202.116.46.215/hsdocker2019/hs_base:v1.0
-
 docker tag hs_hadoop:v1.0 202.116.46.215/hsdocker2019/hs_hadoop:v1.0 
 docker push 202.116.46.215/hsdocker2019/hs_hadoop:v1.0 
 
-docker tag hs_hive:v1.0 202.116.46.215/hsdocker2019/hs_hive:v1.0  
-docker push 202.116.46.215/hsdocker2019/hs_hive:v1.0  
+docker tag hs_hive-hadoop:v1.0 202.116.46.215/hsdocker2019/hs_hive-hadoop:v1.0  
+docker push 202.116.46.215/hsdocker2019/hs_hive-hadoop:v1.0  
 
+docker tag hs_spark-hadoop:v1.0  202.116.46.215/hsdocker2019/hs_spark-hadoop:v1.0  
+docker push 202.116.46.215/hsdocker2019/hs_spark-hadoop:v1.0
 
-docker tag hs_spark:v1.0  202.116.46.215/hsdocker2019/hs_spark:v1.0  
-docker push 202.116.46.215/hsdocker2019/hs_spark:v1.0
+docker tag hs_zk-hadoop:v1.0  202.116.46.215/hsdocker2019/hs_zk-hadoop:v1.0  
+docker push 202.116.46.215/hsdocker2019/hs_zk-hadoop:v1.0
 
 
 docker tag hs_hbase:v1.0 202.116.46.215/hsdocker2019/hs_hbase:v1.0 
