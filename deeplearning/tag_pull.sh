8d798859192fb1b60343423dd68e596c94133392
LPF-OFFICE <hudalpf@163.com>
2019-03-24 21:09:42 +0800

    nvidia-9.0-base-ubuntu16.04

--- deeplearning/tag_pull.sh
+++ deeplearning/tag_pull.sh
@@ -21,3 +21,15 @@ docker tag 202.116.46.215/hsdocker2019/hs_plate-dection:py2_cpu-v1.0 hs_plate-de
 #hs_plate-dection:py2_gpu-v1.0
 docker pull 202.116.46.215/hsdocker2019/hs_plate-dection:py2_gpu-v1.0
 docker tag 202.116.46.215/hsdocker2019/hs_plate-dection:py2_gpu-v1.0 hs_plate-dection:py2_gpu-v1.0
+
+#oraclelinux:7
+docker pull 202.116.46.215/hsdocker2019/oraclelinux:7
+docker tag 202.116.46.215/hsdocker2019/oraclelinux:7 oraclelinux:7 
+
+#python-conda-2.7:v1.0
+docker pull 202.116.46.215/hsdocker2019/python-conda-2.7:v1.0
+docker tag 202.116.46.215/hsdocker2019/python-conda-2.7:v1.0 python-conda-2.7:v1.0 
+
+#python-conda-3:v1.0
+docker pull 202.116.46.215/hsdocker2019/python-conda-3:v1.0
+docker tag 202.116.46.215/hsdocker2019/python-conda-3:v1.0 python-conda-3:v1.0 
