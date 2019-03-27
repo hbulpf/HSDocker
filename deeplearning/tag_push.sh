f6da87ef7987d624db315a88409b92e7f5bd7def
LPF-OFFICE <hudalpf@163.com>
2019-03-27 12:49:03 +0800

    hs_base-notebook:py3

--- deeplearning/tag_push.sh
+++ deeplearning/tag_push.sh
@@ -4,8 +4,8 @@ docker push 202.116.46.215/hsdocker2019/hs_base:ubuntu-16.04
 
 #hs_base:base-notebook
 docker tag jupyter/base-notebook:59b402ce701d 202.116.46.215/hsdocker2019/
-hs_base-notebook:v1.0
-docker push 202.116.46.215/hsdocker2019/hs_base-notebook:v1.0
+hs_base-notebook:py3
+docker push 202.116.46.215/hsdocker2019/hs_base-notebook:py3
 
 #tensorflow:1.12.0-gpu-py3
 docker tag tensorflow/tensorflow:1.12.0-gpu-py3 202.116.46.215/hsdocker2019/tensorflow:1.12.0-gpu-py3 
