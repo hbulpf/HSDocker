7282762632d05b56db87fc2470ac999e8a84351e
LPF-OFFICE <hudalpf@163.com>
2019-03-31 14:42:02 +0800

    #get parameters

--- k8s_apps/hadoop_on_k8s/users_benchmark/start-apps.sh
+++ k8s_apps/hadoop_on_k8s/users_benchmark/start-apps.sh
@@ -1,10 +1,10 @@
-WORKDIR=$(pwd)/
-echo "Working Dir: ${WORKDIR}"
 #get parameters
 ${USERNS}=$1
+WORKDIR=$(pwd)/
+echo "Working Dir: ${WORKDIR}"
 #delete resources if exists
-sed ${WORKDIR}hadoop-master.yaml -e "s/hadoop-/${USERNS}-hadoop-/g" | kubectl delete -n ${USERNS}  -f -
-sed ${WORKDIR}hadoop-slave.yaml -e "s/hadoop-/${USERNS}-hadoop-/g" | kubectl delete -n ${USERNS}  -f -
+sed ${WORKDIR}hadoop-master.yaml -e "s/hadoop-/${USERNS}-hadoop-/g" | kubectl delete -n ${USERNS} -f -
+sed ${WORKDIR}hadoop-slave.yaml -e "s/hadoop-/${USERNS}-hadoop-/g" | kubectl delete -n ${USERNS} -f -
 sed hadoop-web.yaml -e "s/hadoop-/${USERNS}-hadoop-/g" | kubectl delete -n ${USERNS} -f -
 kubectl delete ns ${USERNS}
 #create resources
