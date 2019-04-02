95df75bea02a8e5fdec0dda5a447b3179e363d10
LPF@Dom <lipengfei@hnbdata.com>
2019-04-02 12:04:38 +0800

    aliyun镜像仓库

---
+++ docker_hadoop_spark/docker_registry_push.sh
@@ -0,0 +1,47 @@
+docker tag hs_base:v1.0 hsdocker2019/hs_base:v1.0
+docker push hsdocker2019/hs_base:v1.0
+
+docker tag hs_hadoop:v1.0 hsdocker2019/hs_hadoop:v1.0 
+docker push hsdocker2019/hs_hadoop:v1.0 
+
+docker tag hs_hive-hadoop:v1.0 hsdocker2019/hs_hive-hadoop:v1.0  
+docker push hsdocker2019/hs_hive-hadoop:v1.0  
+
+docker tag hs_spark-hadoop:v1.0  hsdocker2019/hs_spark-hadoop:v1.0  
+docker push hsdocker2019/hs_spark-hadoop:v1.0
+
+docker tag hs_zk-hadoop:v1.0  hsdocker2019/hs_zk-hadoop:v1.0  
+docker push hsdocker2019/hs_zk-hadoop:v1.0
+
+docker tag hs_zk:v1.0  hsdocker2019/hs_zk:v1.0  
+docker push hsdocker2019/hs_zk:v1.0
+
+docker tag hs_hbase-zk-hadoop:v1.0 hsdocker2019/hs_hbase-zk-hadoop:v1.0 
+docker push hsdocker2019/hs_hbase-zk-hadoop:v1.0 
+
+docker tag hs_storm-zk:v1.0 hsdocker2019/hs_storm-zk:v1.0
+docker push hsdocker2019/hs_storm-zk:v1.0
+
+docker tag hs_flume-hadoop:v1.0 hsdocker2019/hs_flume-hadoop:v1.0
+docker push hsdocker2019/hs_flume-hadoop:v1.0
+
+docker tag hs_kafka-zk:v1.0 hsdocker2019/hs_kafka-zk:v1.0
+docker push hsdocker2019/hs_kafka-zk:v1.0
+
+docker tag hs_pig-hadoop:v1.0 hsdocker2019/hs_pig-hadoop:v1.0
+docker push hsdocker2019/hs_pig-hadoop:v1.0
+
+docker tag hs_redis:v1.0 hsdocker2019/hs_redis:v1.0
+docker push hsdocker2019/hs_redis:v1.0
+
+docker tag hs_redis-spark:v1.0 hsdocker2019/hs_redis-spark:v1.0
+docker push hsdocker2019/hs_redis-spark:v1.0
+
+docker tag hs_mahout-hadoop:v1.0 hsdocker2019/hs_mahout-hadoop:v1.0
+docker push hsdocker2019/hs_mahout-hadoop:v1.0
+
+docker tag hs_mongodb:v1.0 hsdocker2019/hs_mongodb:v1.0
+docker push hsdocker2019/hs_mongodb:v1.0
+
+docker tag hs_leveldb:v1.0 hsdocker2019/hs_leveldb:v1.0
+docker push hsdocker2019/hs_leveldb:v1.0
--- docker_hadoop_spark/tag_push.sh
+++ docker_hadoop_spark/tag_push.sh
@@ -1,3 +1,6 @@
+docker tag hs_base:v1.0 202.116.46.215/hsdocker2019/hs_base:v1.0
+docker push 202.116.46.215/hsdocker2019/hs_base:v1.0
+
 docker tag hs_hadoop:v1.0 202.116.46.215/hsdocker2019/hs_hadoop:v1.0 
 docker push 202.116.46.215/hsdocker2019/hs_hadoop:v1.0 
 
